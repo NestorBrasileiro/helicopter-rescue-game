a94776b5b7c520dee6eda85b3a56b8d725817dc7
Nestor Brasileiro <nestor.brasileiro@ozmap.com.br>
2022-01-22 18:34:35 -0300

    HRG-1 feature Adding movement to background and for player;

--- script/js/script.js
+++ script/js/script.js
@@ -2,9 +2,58 @@ function start() { // Inicio da função start()
 
 	$("#start").hide();
 	
-	$("#backgroundGame").append("<div id='player'></div>");
-	$("#backgroundGame").append("<div id='enemy1'></div>");
+	$("#backgroundGame").append("<div id='player' class='anima1' ></div>");
+	$("#backgroundGame").append("<div id='enemy1' class='anima2'></div>");
 	$("#backgroundGame").append("<div id='enemy2'></div>");
-	$("#backgroundGame").append("<div id='friend'></div>");
+	$("#backgroundGame").append("<div id='friend' class='anima3'></div>");
 
-}
+    var game = {};
+    
+    game.timer = setInterval(loop, 30);
+    
+    function loop() {
+        moveBackground();
+        movePlayer();
+    }
+    
+    var KEY = {
+        W: 87,
+        S: 83,
+        D: 68
+        }
+    
+    game.keypressed = [];
+    
+    $(document).keydown(function(e){
+        game.keypressed[e.which] = true;
+    });
+        
+        
+    $(document).keyup(function(e){
+        game.keypressed[e.which] = false;
+    });
+
+    function movePlayer() {
+        
+        if (game.keypressed[KEY.W]) {
+            var topo = parseInt($("#player").css("top"));
+            $("#player").css("top",topo-10);
+        }
+        
+        if (game.keypressed[KEY.S]) {
+            var topo = parseInt($("#player").css("top"));
+            $("#player").css("top",topo+10);	
+        }
+        
+        if (game.keypressed[KEY.D]) {
+            //Chama função Disparo	
+        }
+    
+    }
+    
+    function moveBackground(){
+        esquerda = parseInt($("#backgroundGame").css("background-position"));
+        $("#backgroundGame").css("background-position", esquerda - 1)
+        
+    }
+}    
